73fb7f8920e09f79d4d030a4b2fd49b272763543
johnpaulchukwu43 <johnpaulchukwu43@gmail.com>
2017-03-15 20:57:20 +0100

    Update developer.java

--- app/src/main/java/javadevs/javadevs/developer.java
+++ app/src/main/java/javadevs/javadevs/developer.java
@@ -2,18 +2,21 @@ package javadevs.javadevs;
 
 /**
  * Created by CHUKWU JOHNPAUL on 12/03/17.
+ This Class represents an actual model of the data we are going to be getting 
  */
 
 public class developer {
-    private String Username;
-    private String ImageUrl;
-    private String ProfileUrl;
+    private String Username;//Username of the developer
+    private String ImageUrl;//Their Image
+    private String ProfileUrl;//Profile Url
 
+    //Constructor to initialize values of fields
     public developer(String username, String imageUrl, String profileUrl) {
         Username = username;
         ImageUrl = imageUrl;
         ProfileUrl = profileUrl;
     }
+    //Get Methods to access the respective fields
 
     public String getUsername() {
         return Username;
